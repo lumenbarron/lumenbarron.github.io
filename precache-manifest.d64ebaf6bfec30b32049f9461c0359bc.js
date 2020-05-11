9aae3a7abfc315398a380ea8df13ed9db94b0b0d
lumenbarron <lucemendez@gmail.com>
2020-05-10 21:41:52 -0500

    Updates

---
+++ precache-manifest.d64ebaf6bfec30b32049f9461c0359bc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b06a46c51b816877822fca96de1f614",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5543b52986697695fd8b",
+    "url": "/static/css/main.5b5d0765.chunk.css"
+  },
+  {
+    "revision": "2e372c46464791061147",
+    "url": "/static/js/2.389ed403.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.389ed403.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5543b52986697695fd8b",
+    "url": "/static/js/main.190e798b.chunk.js"
+  },
+  {
+    "revision": "4fba741fb7176f89c72d",
+    "url": "/static/js/runtime-main.ac97a943.js"
+  },
+  {
+    "revision": "bf6f5f7df3d3d7ce03b82108677610e2",
+    "url": "/static/media/banner-mobile-lucy.bf6f5f7d.jpg"
+  },
+  {
+    "revision": "ab94ba311fa32454bc3c51ff9fd00589",
+    "url": "/static/media/banner-web-lucy.ab94ba31.jpg"
+  },
+  {
+    "revision": "71ce40b60eaef3df073ea5e128b0fe34",
+    "url": "/static/media/bikely.71ce40b6.gif"
+  },
+  {
+    "revision": "0f288f6b101752e154fda717d69a5cbb",
+    "url": "/static/media/foodmap-gif.0f288f6b.gif"
+  },
+  {
+    "revision": "b1bd463f8ffd99531a33ed2b90b29b26",
+    "url": "/static/media/il-piccolo.b1bd463f.png"
+  },
+  {
+    "revision": "39b1da16042a04e351fa3bcee5632266",
+    "url": "/static/media/lucyName.39b1da16.png"
+  },
+  {
+    "revision": "9e4a4509fc00a306729acb0f48ef93bb",
+    "url": "/static/media/pixar-3.9e4a4509.gif"
+  },
+  {
+    "revision": "da6b07339154f03d5d7d4f174cbe978f",
+    "url": "/static/media/pokemon-home-gif.da6b0733.gif"
+  },
+  {
+    "revision": "68f2330e8d7ebd904afbd081e0ef5183",
+    "url": "/static/media/share-smile.68f2330e.png"
+  },
+  {
+    "revision": "7cd0d14cc00d0ae332b8b354dd9e2498",
+    "url": "/static/media/starbucks.7cd0d14c.gif"
+  },
+  {
+    "revision": "888f5a0c1a000c125b95b9f2726eed60",
+    "url": "/static/media/vitalinea.888f5a0c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.35325ffea1eb286c4245ec3c38a24d75.js"
+  "/precache-manifest.d64ebaf6bfec30b32049f9461c0359bc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
